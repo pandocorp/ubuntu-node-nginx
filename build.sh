df253595a5b12c97a163bbc291147d81d793fb4a
gramcha <gramcha@pando.in>
2023-02-01 16:33:54 +0530

    building node 14.21.2 image

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 version=3.0.1
 docker buildx build --push --tag gramcha/ubuntu-node-nginx:$version -f Dockerfile-node18 -o type=image --platform=linux/arm64,linux/amd64 .
+
+
+#version=2.0.1
+#docker buildx build --push --tag gramcha/ubuntu-node-nginx:$version -f Dockerfile-node14 -o type=image --platform=linux/arm64,linux/amd64 .
