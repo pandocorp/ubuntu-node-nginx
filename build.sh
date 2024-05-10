572f6e7f82c82b1f9cc19e9bdee3b2f6a60b1fd3
gramcha <gramcha@pando.in>
2024-05-10 08:47:36 +0530

    ubuntu 22.04 update

--- build.sh
+++ build.sh
@@ -11,5 +11,9 @@
 #docker buildx build --push --tag gramcha/ubuntu-node-nginx:$version -f Dockerfile-node18-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
 
 
-version=5.0.0
+#version=5.0.0
+#docker buildx build --push --tag gramcha/ubuntu-node-nginx:$version -f Dockerfile-node18-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
+
+# ubuntu 22.04, node v18.20.2, nginx 1.18.0, consul 1.9.4
+version=6.0.0
 docker buildx build --push --tag gramcha/ubuntu-node-nginx:$version -f Dockerfile-node18-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
