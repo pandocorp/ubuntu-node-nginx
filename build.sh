df58f74916870af22ba1fe2ef99c9910dfd905c6
gramcha <gramcha@pando.in>
2024-06-19 12:12:56 +0530

    adding weasyprint and pm2logrotate

--- build.sh
+++ build.sh
@@ -15,5 +15,9 @@
 #docker buildx build --push --tag gramcha/ubuntu-node-nginx:$version -f Dockerfile-node18-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
 
 # ubuntu 22.04, node v18.20.2, nginx 1.18.0, consul 1.9.4
-version=6.0.0
+#version=6.0.0
+#docker buildx build --push --tag gramcha/ubuntu-node-nginx:$version -f Dockerfile-node18-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
+
+# ubuntu 22.04, node v18.20.2, nginx 1.18.0, consul 1.9.4, weasyprint
+version=6.0.1
 docker buildx build --push --tag gramcha/ubuntu-node-nginx:$version -f Dockerfile-node18-nginx-consul -o type=image --platform=linux/arm64,linux/amd64 .
